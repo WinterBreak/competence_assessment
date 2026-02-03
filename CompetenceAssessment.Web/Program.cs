59cb5aa8ba975b5cb5f599f2f25b49f6cf73b0b7
Elizaveta Lozhkina <liza.lojkina@yandex.ru>
2026-02-03 17:50:50 +0500

    Set contexts

--- CompetenceAssessment.Web/Program.cs
+++ CompetenceAssessment.Web/Program.cs
@@ -1,9 +1,21 @@
+using CompetenceAssessment.Infrastructure;
+using CompetenceAssessment.Infrastructure.UserManagement;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
 builder.Services.AddControllers();
 builder.Services.AddControllersWithViews();
+
+// TODO LazyProxies
+var connectionString = builder.Configuration.GetConnectionString("CompetenceAssessment");
+builder.Services.AddDbContext<UserManagementContext>(options =>
+    options.UseNpgsql(connectionString));
+builder.Services.AddDbContext<AssessmentContext>(options =>
+    options.UseNpgsql(connectionString));
+
 
 var app = builder.Build();
 
